5f0eb06e915e32edfd4f36db7244aa5473069b2c
dvmcfall <dvmcfall@gmail.com>
2016-07-29 13:02:18 -0400

    WrapValueInElement improved

--- FallDave.Trifles.Xml/XPath/TriflesXPathData.cs
+++ FallDave.Trifles.Xml/XPath/TriflesXPathData.cs
@@ -57,7 +57,7 @@ namespace FallDave.Trifles.Xml.XPath
         /// <returns></returns>
         public static XPathNodeIterator ToNodeSet(IEnumerable<object> elements)
         {
-            var items = elements.Select(element => WrapXPathValueInXElement(element));
+            var items = elements.Select(element => WrapValueInElement(element));
             var root = new XElement("root", items.ToArray());
             var rootNav = root.CreateNavigator();
             var result = (XPathNodeIterator)rootNav.Evaluate("item");
@@ -67,24 +67,106 @@ namespace FallDave.Trifles.Xml.XPath
 
 
 
+
+
         /// <summary>
-        /// Encodes an object value as an <see cref="XElement"/> having the name "item" and some
-        /// additional information that might be helpful in round-tripping the value.
-        /// </summary>
-        /// <param name="value"></param>
+        /// Encodes an object value as an <see cref="XElement"/> having the name
+        /// "item" and some additional information that might be helpful in
+        /// round-tripping the value.
+        /// <para>
+        /// The produced <see cref="XElement"/> will have at least the property
+        /// <c>clr-type</c>, which is the <see cref="System.Type.FullName"/>
+        /// given by its type, or simply <c>null</c> if the value is
+        /// <c>null</c>.
+        /// </para>
+        /// <para>
+        /// All values produce an element with its <c>clr-type</c> attribute set
+        /// to the <see cref="System.Type.FullName"/> for its type, or
+        /// <c>null</c> if the value is <c>null</c>. Values of a few recognized
+        /// types also have a <c>type</c> attribute, which is the general type
+        /// of XPath value it is meant to represent, and some also have other
+        /// attributes.
+        /// <list type="bulleted">
+        /// <item>
+        /// A <c>bool</c> value has the type <c>boolean</c>. Its string contents
+        /// are empty for <c>false</c> or <c>true</c> for true. Use XPath
+        /// <c>boolean()</c> on the string to restore the value.
+        /// </item>
+        /// <item>
+        /// A <c>char</c> value has the type <c>string</c>, with the
+        /// <c>subtype</c> attribute set to <c>char</c> and the
+        /// <c>numeric-value</c> attribute as the numeric value (use XPath
+        /// <c>number()</c> to restore).
+        /// </item>
+        /// <item>
+        /// A value of any of the numeric simple types (e.g. <c>int</c>,
+        /// <c>double</c>, ...) has the type <c>number</c>, with the
+        /// <c>subtype</c> attribute set to <c>floating-point</c> (subset of
+        /// <c>double</c>), <c>signed-integer</c> (subset of <c>long</c>),
+        /// <c>unsigned-integer</c> (subset of <c>ulong</c>), or <c>decimal</c>
+        /// (<c>decimal</c> values). Its string value is as precise a
+        /// representation of the number as can be supplied by the system, and
+        /// no exponential notation is used. While the XPath <c>number()</c>
+        /// function accepts the value produced, XPath numbers are
+        /// floating-point only (corresponding to <c>double</c>) and may not
+        /// retain the full precision of the value (large decimal or integer
+        /// values are likely to be approximated). However, treating the value
+        /// as a string causes no loss of data, so retaining the string value
+        /// and <c>subtype</c> or <c>clr-type</c> attributes allows the full
+        /// value and its type to be restored outside of XPath.
+        /// </item>
+        /// <item>
+        /// An <see cref="XPathNavigator"/> (or <see cref="IXPathNavigable"/>)
+        /// value has the type <c>result-tree-fragment</c>. The navigator's
+        /// contents are expanded inside the body of the element.
+        /// </item>
+        /// <item>
+        /// A <see cref="XPathNodeIterator"/> value has the type
+        /// <c>node-set</c>. The iterator's contents are expanded inside the
+        /// body of the element.
+        /// </item>
+        /// <item>
+        /// The value <c>null</c> has the type <c>node-set</c>, with the
+        /// <c>subtype</c> attribute set to <c>null</c>, the
+        /// <c>{http://www.w3.org/2001/XMLSchema-instance}nil</c> (usually known
+        /// as <c>xsi:nil</c>) set to <c>true</c>, and an empty body.
+        /// </item>
+        /// <item>
+        /// Any other value (if <paramref name="returnNullInsteadOfUnknown"/> is
+        /// <c>false</c>) has the type <c>unknown</c>, with the result of its
+        /// <see cref="object.ToString()"/> method as the string contents. This
+        /// is intended more for debugging purposes than for actual production
+        /// utility; it more than likely does not contain all information needed
+        /// to restore the value. (To implement wrapping for unsupported type,
+        /// consider serializing the value to a document and wrap an <see
+        /// cref="XPathNodeIterator"/> that retrieves the document.)
+        /// </item>
+        /// </list>
+        /// </para>
+        /// <param name="value">The value to encode</param>
+        /// <param name="elementName">
+        /// The name of the element to produce (default is <c>item</c>)
+        /// </param>
+        /// <param name="returnNullInsteadOfUnknown">
+        /// If <c>true</c>, an unknown value will result in <c>null</c> instead
+        /// of an <c>unknown</c>-typed element (default is <c>false</c>)</param>
         /// <returns></returns>
-        public static XElement WrapXPathValueInXElement(object value)
+        public static XElement WrapValueInElement(object value, XName elementName = null, bool returnNullInsteadOfUnknown = false)
         {
+            elementName = elementName ?? "item";
+
+            var valueClrType = (value == null) ? null : value.GetType();
+
             if (value is bool)
             {
                 var boolValue = (bool)value;
-                return boolValue ? CreateWrapItemElement("boolean", "true") : CreateWrapItemElement("boolean");
+                return boolValue ? CreateWrapItemElement(elementName, valueClrType, "boolean", "true") : CreateWrapItemElement(elementName, valueClrType, "boolean");
             }
             else if (value is char)
             {
                 var convValue = (char)value;
 
-                return CreateWrapItemElement("string",
+                return CreateWrapItemElement(elementName, valueClrType, "string",
                     new XAttribute("subtype", "char"),
                     new XAttribute("numeric-value", (int)convValue),
                     convValue.ToString());
@@ -105,33 +187,51 @@ namespace FallDave.Trifles.Xml.XPath
 
                 content.Add(xpathStringValue);
 
-                return CreateWrapItemElement("number", content.ToArray());
+                return CreateWrapItemElement(elementName, valueClrType, "number", content.ToArray());
             }
             else if (value is string)
             {
-                return CreateWrapItemElement("string", value);
+                return CreateWrapItemElement(elementName, valueClrType, "string", value);
             }
             else if (value is XPathNodeIterator)
             {
                 var i = (XPathNodeIterator)value;
                 var nodes = i.AsEnumerable().Select(n => n.ToXNode()).ToArray();
 
-                return CreateWrapItemElement("node-set", nodes);
+                return CreateWrapItemElement(elementName, valueClrType, "node-set", nodes);
             }
             else if (value is IXPathNavigable)
             {
                 var xnode = ((IXPathNavigable)value).ToXNode();
 
-                return CreateWrapItemElement("result-tree-fragment", xnode);
+                return CreateWrapItemElement(elementName, valueClrType, "result-tree-fragment", xnode);
             }
             else if (value == null)
             {
-                return CreateWrapItemElement("node-set", new XAttribute("subtype","null"));
+                var xe = CreateWrapItemElement(elementName, valueClrType, "node-set", new XAttribute("subtype","null"));
+                xe.XsiNil(true);
+                return xe;
             }
             else
             {
-                throw new XPathException("Converting value of type " + value.GetType().FullName + " is not supported.");
+                if(returnNullInsteadOfUnknown)
+                {
+                    return null;
+                }
+                return CreateWrapItemElement(elementName, valueClrType, "unknown", value.ToString());
             }
+        }
+
+        /// <summary>
+        /// Retrieves <see cref="WrapValueInElement(object, XName, bool)"/>, but returns empty for an unknown value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="elementName"></param>
+        /// <returns></returns>
+        public static Opt<XElement> WrapValueInElementOpt(object value, XName elementName = null)
+        {
+            var xe = WrapValueInElement(value, elementName, true);
+            return Opt.FullIfNotNull(xe);
         }
 
         private static string GetNumericSubtype(IConvertible convValue)
@@ -159,10 +259,11 @@ namespace FallDave.Trifles.Xml.XPath
         }
 
         // Creates an XElement containing the data of a node-set item.
-        private static XElement CreateWrapItemElement(string type, params object[] otherContent)
+        private static XElement CreateWrapItemElement(XName name, Type clrType, string type, params object[] otherContent)
         {
             var typeAttr = new XAttribute("type", type);
-            var elem = new XElement("item", typeAttr);
+            var clrTypeAttr = new XAttribute("clr-type", clrType == null ? "null" : clrType.FullName);
+            var elem = new XElement(name, typeAttr, clrTypeAttr);
             elem.Add(otherContent);
             return elem;
         }
